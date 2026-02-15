9aab76a3c0ae529b5179d5f8a3a99266388d3889
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-02-15 18:22:13 -0300

    Uploading project

---
+++ AI-Expense-Analyzer/AI-Expense-Analyzer/AI_Expense_AnalyzerApp.swift
@@ -0,0 +1,21 @@
+//
+//  AI_Expense_AnalyzerApp.swift
+//  AI-Expense-Analyzer
+//
+//  Created by Bianca Maciel on 15/02/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct AI_Expense_AnalyzerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ AI-Expense-Analyzer/AI-Expense-AnalyzerTests/AI_Expense_AnalyzerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Expense_AnalyzerTests.swift
+//  AI-Expense-AnalyzerTests
+//
+//  Created by Bianca Maciel on 15/02/26.
+//
+
+import Testing
+@testable import AI_Expense_Analyzer
+
+struct AI_Expense_AnalyzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
