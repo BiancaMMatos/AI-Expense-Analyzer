8b63bcada8f26e6aca6ce476d8ca9444eb5f3c2f
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-03-11 12:55:04 -0300

    Changing to only read value

--- AI-Expense-Analyzer/AI-Expense-Analyzer/Presentation/AddExpenses/AddExpenseView.swift
+++ AI-Expense-Analyzer/AI-Expense-Analyzer/Presentation/AddExpenses/AddExpenseView.swift
@@ -14,93 +14,96 @@ struct AddExpenseView: View {
     @Environment(\.colorScheme) private var colorScheme
     
     var body: some View {
-        ZStack {
-            Color(.systemBackground)
-            VStack(alignment: .center) {
-                Text("New Expense")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.trailing, 200)
-                
-                Button {
-                    print("camera button pressed")
+        ScrollView {
+            ZStack {
+                Color(.systemBackground)
+                VStack(alignment: .center) {
+                    Text("New Expense")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.trailing, 200)
                     
-                } label: {
-                    Image(systemName: "camera.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 25, height: 25)
-                        .foregroundStyle(colorScheme == .dark ? .white : .black)
-                        .padding(20)
-                        .background(Color(.systemGray5))
-                        .clipShape(Circle())
-                        .shadow(radius: 5.0)
-                }
-                
-                Form {
-                    Section {
-                        TextField("Value", text: $value)
-                            .font(.callout)
-                            .padding()
+                    Button {
+                        print("camera button pressed")
                         
-                    } header: {
-                        Text("Value Info")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(colorScheme == .dark ? .white : .gray)
+                    } label: {
+                        Image(systemName: "camera.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(colorScheme == .dark ? .white : .black)
+                            .padding(20)
+                            .background(Color(.systemGray5))
+                            .clipShape(Circle())
+                            .shadow(radius: 5.0)
                     }
-
-                }
-                .scrollContentBackground(.hidden)
-                .background(Color(.systemGray5))
-                .padding(20)
-                .clipShape(RoundedRectangle(cornerRadius: 80))
-            
-                
-                LazyVGrid(columns: [
-                    GridItem(.flexible()),
-                    GridItem(.flexible()),
-                    GridItem(.flexible())
-                ], spacing: 30.0) {
-                    ForEach(Category.categories, id: \.self) { category in
-                        Button {
-                            print("category button selected")
-                            viewModel.selectCategoryFromImage()
+                    
+                    Form {
+                        Section {
+                            Text(value)
+                                .font(.callout)
+                                .padding()
                             
-                        } label: {
-                            Image(systemName: category.icon)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 25, height: 25)
-                                .foregroundStyle(colorScheme == .dark ? .white : .black)
-                                .padding(20)
-                                .background(Color(.systemGray5))
-                                .clipShape(Circle())
-                                .shadow(radius: 5.0)
+                        } header: {
+                            Text("Value Informed")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(colorScheme == .dark ? .white : .gray)
+                        }
+                        
+                    }
+                    .scrollContentBackground(.hidden)
+                    .background(Color(.systemGray5))
+                    .padding(20)
+                    .clipShape(RoundedRectangle(cornerRadius: 80))
+                    
+                    
+                    LazyVGrid(columns: [
+                        GridItem(.flexible()),
+                        GridItem(.flexible()),
+                        GridItem(.flexible())
+                    ], spacing: 30.0) {
+                        ForEach(Category.categories, id: \.self) { category in
+                            Button {
+                                print("category button selected")
+                                viewModel.selectCategoryFromImage()
+                                
+                            } label: {
+                                Image(systemName: category.icon)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 25, height: 25)
+                                    .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                    .padding(20)
+                                    .background(Color(.systemGray5))
+                                    .clipShape(Circle())
+                                    .shadow(radius: 5.0)
+                            }
                         }
                     }
-                }
-                .padding(50)
-                
-                Button {
-                    print("Saved")
-                    viewModel.saveExpense()
+                    .padding(50)
                     
-                } label: {
-                    ZStack {
+                    Button {
+                        print("Saved")
+                        viewModel.saveExpense()
                         
-                        Text("Save")
-                            .font(.title3)
-                            .fontWeight(.medium)
-                            .tint(.black)
-                            .padding(.horizontal, 50)
-                            .padding(15)
-                            .background(Color(.systemYellow))
-                            .clipShape(RoundedRectangle(cornerRadius: 50.0))
-                            .accessibilityIdentifier("addExpense:saveButton")
+                    } label: {
+                        ZStack {
+                            
+                            Text("Save")
+                                .font(.title3)
+                                .fontWeight(.medium)
+                                .tint(.black)
+                                .padding(.horizontal, 50)
+                                .padding(15)
+                                .background(Color(.systemYellow))
+                                .clipShape(RoundedRectangle(cornerRadius: 50.0))
+                                .accessibilityIdentifier("addExpense:saveButton")
+                        }
                     }
+                    
+                    
                 }
-
                 
             }
             
