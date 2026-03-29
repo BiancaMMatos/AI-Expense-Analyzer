e445ed19e63337673d9247cd015a5ef07a1ffb8d
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-03-29 17:24:07 -0300

    Adding errors structures

--- AI-Expense-Analyzer/AI-Expense-Analyzer/Data/Services/OCRService.swift
+++ AI-Expense-Analyzer/AI-Expense-Analyzer/Data/Services/OCRService.swift
@@ -5,13 +5,36 @@
 //  Created by Bianca Maciel on 29/03/26.
 //
 
+import UIKit
 import Vision
-import SwiftUI
+
 
 final class VisionService: OCRService {
     func extractText(from image: UIImage) async throws -> String {
-        // TODO: Add logic
+        
+        /// Preparing image; converting UIImage to CGImage
+        
+        
+        /// Creating text request
+        
+        
+        /// Executing request
+        
+        
+        /// Extracting text
+        
+        
+        /// Normalizing text
+        
+        
+        /// Handling errors
+        
+        
         return ""
+    }
+    
+    private func normalize() {
+        
     }
     
     
